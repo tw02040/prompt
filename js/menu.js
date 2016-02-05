0d559eb9af120db06825a6b17cc29d46b098bd79
tw02040 <tw02040@naver.com>
2016-02-05 12:04:34 +0900

    마지막이였으면..

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(function(){
+				$('.btnMenu').toggle( 
+			    // 우측 메뉴와 센터 컨텐츠를 margin-left -260px 만큼 이동
+				    function() {
+				     fn_resizeHeight();
+				     $('#slideRight').removeClass('blind').addClass('on').animate({ right: 0 }, 'fast', function() { });
+				     $('#wrap').animate({ 'margin-left' : '-260px' }, 'fast', function() {  });  
+				    }, 
+				    function() {
+				     $('#wrap').animate({ 'margin-left' : '0' }, 'fast', function() { });
+				     $('#slideRight').animate({ right: -260 }, 'fast', function() { }).queue(function(){
+				      $(this).removeClass('on').addClass('blind');
+				      $(this).dequeue();
+				     });
+			    	}
+			   );
+			});
+			//좌,우측 페이지 scrren 세로사이즈에 맞게 리사이징
+			fn_resizeHeight = function() {
+				var sideHeight = $(document).height(); 
+				$('.sideMenu').css({'height':sideHeight});
+			}
